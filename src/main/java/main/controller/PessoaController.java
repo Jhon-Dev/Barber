0997a227a9bc90aea234c6e02641d5ff2013676a
Jhonatan Nuss <jhonatan.nuss082897@gmail.com>
2020-11-12 01:13:47 -0300

    Add método excluir

--- src/main/java/main/controller/PessoaController.java
+++ src/main/java/main/controller/PessoaController.java
@@ -2,6 +2,8 @@ package main.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -37,6 +39,14 @@ public class PessoaController {
 		return modelAndView;
 	}
 	
+	@GetMapping("/removerpessoa/{idpessoa}")
+	public ModelAndView excluir(@PathVariable("idpessoa") Long idpessoa) {	
+		
+		pessoaRepository.deleteById(idpessoa);
+		ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
+		modelAndView.addObject("pessoas", pessoaRepository.findAll());
+		modelAndView.addObject("pessoaobj", new Pessoa());
 
-	
+		return modelAndView;
+	}
 }
